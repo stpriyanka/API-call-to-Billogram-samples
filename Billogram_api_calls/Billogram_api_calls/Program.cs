84c0398583c83a57e8bdd32597bca309525cc050
Safwath Priyanka <stpriyanka2011@gmail.com>
2016-09-20 15:33:00 +0200

    Unlocked Billogram API:
    # Create customer(per organization) to whom billogram would be sent
    # Get a single customer by customer no
    # Create billogram(invoice) as an unattested state and save in draft
    # Fetch single billogram(invoice)
    # List of billograms(invoices)  [search by page number and size]
    # Get list of billograms by conditions (unpaid)
    # Get list of billograms by conditions (unattested)
    # Get list of billograms by conditions (paid)
    # Get list of billograms by customer no
    #Send billogram(unattested: those are created only and save in draft) using billogram id .(status become 'unpaid' from 'unattested')

---
+++ Billogram_api_calls/Billogram_api_calls/Program.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Billogram_api_calls
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			RunAsync_send_billogram().Wait();
+		}
+
+		/// <summary>
+		/// GET
+		/// </summary>
+		/// <returns></returns>
+		private static async Task RunAsync_get_single_customer()
+		{
+			const string authUser = "3331-0lgWtglW";
+			const string authKey = "761b54eb59aeca57e1a8cfa16fe67693";
+
+			const int customerId = 1;
+
+			var client = new HttpClient();
+
+			var byteArray = Encoding.ASCII.GetBytes(authUser + ":" + authKey);
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			HttpResponseMessage response = await client.GetAsync("https://sandbox.billogram.com/api/v2/customer/" + customerId);
+
+			response.EnsureSuccessStatusCode();
+
+			var responseBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(responseBody);
+
+			Console.ReadLine();
+
+		}
+
+		//404
+		private static async Task RunAsync_Connection_to_api()
+		{
+			const string authUser = "3331-0lgWtglW";
+			const string authKey = "761b54eb59aeca57e1a8cfa16fe67693";
+
+			var client = new HttpClient();
+
+
+			var byteArray = Encoding.ASCII.GetBytes(authUser + ":" + authKey);
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			HttpResponseMessage response = await client.GetAsync("https://sandbox.billogram.com/api/v2/");
+
+			response.EnsureSuccessStatusCode();
+
+			var responseBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(responseBody);
+
+			Console.ReadLine();
+
+		}
+
+		/// <summary>
+		/// POST
+		/// </summary>
+		/// <returns></returns>
+		private static async Task RunAsync_create_a_customer()
+		{
+			const string authUser = "3331-0lgWtglW";
+			const string authKey = "761b54eb59aeca57e1a8cfa16fe67693";
+
+			var Data = new
+			{
+				customer_no = 8,
+				name = "customer8",
+				company_type = "individual",
+				org_no = "",
+				contact = new
+				{
+					name = "customer8",
+					email = "stpriyanka2011@gmail.com"
+				},
+				address = new
+				{
+					street_address = "Armegatan",
+					zipcode = 17171,
+					city = "kista",
+					country = "SE"
+				},
+			};
+
+
+			var client = new HttpClient();
+
+			var byteArray = Encoding.ASCII.GetBytes(authUser + ":" + authKey);
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			HttpResponseMessage response = await client.PostAsJsonAsync("https://sandbox.billogram.com/api/v2/customer/", Data);
+
+			response.EnsureSuccessStatusCode();
+
+			var responseBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(JsonConvert.DeserializeObject(responseBody));
+
+
+			Console.ReadLine();
+		}
+
+		/// <summary>
+		/// POST {save in draft}
+		/// </summary>
+		/// <returns></returns>
+		private static async Task RunAsync_create_billogram_as_Unattested_state()
+		{
+
+			const string authUser = "3331-0lgWtglW";
+			const string authKey = "761b54eb59aeca57e1a8cfa16fe67693";
+
+			var data = new
+			{
+				customer = new
+				{
+					customer_no = 1
+				},
+				items = new[]
+				{
+     				new { count = 1, discount = 0, item_no = 1 }
+				}
+			};
+
+
+			var client = new HttpClient();
+
+			var byteArray = Encoding.ASCII.GetBytes(authUser + ":" + authKey);
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			HttpResponseMessage response = await client.PostAsJsonAsync("https://sandbox.billogram.com/api/v2/billogram/", data);
+
+			response.EnsureSuccessStatusCode();
+
+			var responseBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(JsonConvert.DeserializeObject(responseBody));
+
+
+			Console.ReadLine();
+
+		}
+
+		private static async Task RunAsync_send_billogram()
+		{
+
+			const string authUser = "3331-0lgWtglW";
+			const string authKey = "761b54eb59aeca57e1a8cfa16fe67693";
+
+			var data = new
+			{
+				method = "Email"
+			};
+
+			const string unattestedBillogramId = "49sEHHY";
+
+			var queryString = unattestedBillogramId + "/command/send/";
+			var client = new HttpClient();
+
+			var byteArray = Encoding.ASCII.GetBytes(authUser + ":" + authKey);
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			HttpResponseMessage response = await client.PostAsJsonAsync("https://sandbox.billogram.com/api/v2/billogram/" + queryString, data);
+
+			response.EnsureSuccessStatusCode();
+
+			var responseBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(JsonConvert.DeserializeObject(responseBody));
+
+
+			Console.ReadLine();
+
+		}
+
+
+
+
+
+
+		/// <summary>
+		/// GET
+		/// </summary>
+		/// <returns></returns>
+		private static async Task RunAsync_fetch_single_billogram()
+		{
+			const string authUser = "3331-0lgWtglW";
+			const string authKey = "761b54eb59aeca57e1a8cfa16fe67693";
+
+			const string billogramId = "QPsMRVY";
+
+
+			var client = new HttpClient();
+
+			var byteArray = Encoding.ASCII.GetBytes(authUser + ":" + authKey);
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			HttpResponseMessage response = await client.GetAsync("https://sandbox.billogram.com/api/v2/billogram/" + billogramId);
+
+			response.EnsureSuccessStatusCode();
+
+			var responseBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(JsonConvert.DeserializeObject(responseBody));
+
+
+			Console.ReadLine();
+
+		}
+
+
+		/// <summary>
+		/// GET
+		/// </summary>
+		/// <returns></returns>
+		private static async Task RunAsync_fetch_list_of_billograms()
+		{
+			const string authUser = "3331-0lgWtglW";
+			const string authKey = "761b54eb59aeca57e1a8cfa16fe67693";
+
+			var client = new HttpClient();
+
+			var byteArray = Encoding.ASCII.GetBytes(authUser + ":" + authKey);
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			const string baseUrl = "https://sandbox.billogram.com/api/v2/billogram/";
+
+			var query = HttpUtility.ParseQueryString(string.Empty);
+			query["page"] = "1";
+			query["page_size"] = "50";
+			string queryString = query.ToString();
+
+			HttpResponseMessage response = await client.GetAsync(baseUrl + "?" + queryString);
+
+			response.EnsureSuccessStatusCode();
+
+			var responseBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(JsonConvert.DeserializeObject(responseBody));
+
+
+			Console.ReadLine();
+
+		}
+
+
+		/// <summary>
+		/// GET
+		/// </summary>
+		/// <returns></returns>
+		private static async Task RunAsync_get_billograms_list_by_unpaid_state()
+		{
+			const string authUser = "3331-0lgWtglW";
+			const string authKey = "761b54eb59aeca57e1a8cfa16fe67693";
+
+			var client = new HttpClient();
+
+			var byteArray = Encoding.ASCII.GetBytes(authUser + ":" + authKey);
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			const string baseUrl = "https://sandbox.billogram.com/api/v2/billogram/";
+
+			var query = HttpUtility.ParseQueryString(string.Empty);
+			query["page"] = "1";
+			query["page_size"] = "50";
+			query["filter_type"] = "field";
+			query["filter_field"] = "state";
+			query["filter_value"] = "unpaid";
+			string queryString = query.ToString();
+
+			HttpResponseMessage response = await client.GetAsync(baseUrl + "?" + queryString);
+
+			response.EnsureSuccessStatusCode();
+
+			var responseBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(JsonConvert.DeserializeObject(responseBody));
+
+			Console.ReadLine();
+
+		}
+
+		/// <summary>
+		/// GET
+		/// </summary>
+		/// <returns></returns>
+		private static async Task RunAsync_get_billograms_list_by_unattested_state()
+		{
+			const string authUser = "3331-0lgWtglW";
+			const string authKey = "761b54eb59aeca57e1a8cfa16fe67693";
+
+			var client = new HttpClient();
+
+			var byteArray = Encoding.ASCII.GetBytes(authUser + ":" + authKey);
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			const string baseUrl = "https://sandbox.billogram.com/api/v2/billogram/";
+
+			var query = HttpUtility.ParseQueryString(string.Empty);
+			query["page"] = "1";
+			query["page_size"] = "50";
+			query["filter_type"] = "field";
+			query["filter_field"] = "state";
+			query["filter_value"] = "unattested";
+			string queryString = query.ToString();
+
+			HttpResponseMessage response = await client.GetAsync(baseUrl + "?" + queryString);
+
+			response.EnsureSuccessStatusCode();
+
+			var responseBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(JsonConvert.DeserializeObject(responseBody));
+
+			Console.ReadLine();
+
+		}
+
+		/// <summary>
+		/// GET
+		/// </summary>
+		/// <returns></returns>
+		private static async Task RunAsync_get_billograms_list_by_customerNo()
+		{
+			const string authUser = "3331-0lgWtglW";
+			const string authKey = "761b54eb59aeca57e1a8cfa16fe67693";
+
+			var client = new HttpClient();
+
+			var byteArray = Encoding.ASCII.GetBytes(authUser + ":" + authKey);
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			const string baseUrl = "https://sandbox.billogram.com/api/v2/billogram/";
+
+			var query = HttpUtility.ParseQueryString(string.Empty);
+			query["page"] = "1";
+			query["page_size"] = "50";
+			query["filter_type"] = "field";
+			query["filter_field"] = "customer:customer_no";
+			query["filter_value"] = "1";
+			string queryString = query.ToString();
+
+			HttpResponseMessage response = await client.GetAsync(baseUrl + "?" + queryString);
+
+			response.EnsureSuccessStatusCode();
+
+			var responseBody = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(JsonConvert.DeserializeObject(responseBody));
+
+			Console.ReadLine();
+
+		}
+
+
+	}
+}
